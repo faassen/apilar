7c36ead1bc6a4950db563421a9d9fa4e595c2617
Martijn Faassen <faassen@startifact.com>
2022-07-26 15:57:28 +0200

    Barebones UI setup

---
+++ ui/src/App.tsx
@@ -0,0 +1,12 @@
+import type { Component } from "solid-js";
+
+const App: Component = () => {
+  return (
+    <>
+      <h1>Hello world!!!!</h1>
+      <p class="text-4xl text-red-400 tracking-widest">Something</p>
+    </>
+  );
+};
+
+export default App;
