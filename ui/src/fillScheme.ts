f6c5b116d6859ba688098d78cd0cd57f3693e252
Martijn Faassen <faassen@startifact.com>
2022-08-06 19:19:41 +0200

    Various new fill schemes

--- ui/src/fillScheme.ts
+++ ui/src/fillScheme.ts
@@ -2,24 +2,43 @@ import { Location } from "./world";
 import { sequentialHexes } from "./colors";
 
 const RED = 0xff0000;
+const WHITE = 0xffffff;
 
-export function getDefaultFill(location: Location): number {
+export type FillScheme = (location: Location) => number;
+
+function getDefaultFill(location: Location): number {
   if (location.computer != null) {
     return RED;
   }
   return getFreeResourcesFill(location);
 }
 
-export function getFreeResourcesFill(location: Location): number {
-  const max = 500;
-  const resources =
-    location.freeResources > max ? max - 1 : location.freeResources;
-  return sequentialHexes.Blues[9][Math.floor(resources / (max / 9))];
+function convert(colors: number[], value: number, max: number): number {
+  const maxedResources = value >= max ? max - 1 : value;
+  return colors[Math.floor(maxedResources / (max / colors.length))];
 }
 
-export type FillScheme = (location: Location) => number;
+function getFreeResourcesFill(location: Location): number {
+  return convert(sequentialHexes.Blues[9], location.freeResources, 500);
+}
+
+function getMemoryPerComputer(location: Location): number {
+  if (location.computer == null) {
+    return WHITE;
+  }
+  return convert(sequentialHexes.Reds[9], location.computer.memorySize, 1000);
+}
+
+function getProcessorsPerComputer(location: Location): number {
+  if (location.computer == null) {
+    return WHITE;
+  }
+  return convert(sequentialHexes.Purples[9], location.computer.processors, 10);
+}
 
 export const fillSchemes: { [key: string]: FillScheme } = {
   default: getDefaultFill,
-  "free-resources": getFreeResourcesFill,
+  "free resources": getFreeResourcesFill,
+  "memory per computer": getMemoryPerComputer,
+  "processors per computer": getProcessorsPerComputer,
 };
